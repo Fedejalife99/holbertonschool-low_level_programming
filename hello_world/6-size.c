9338856d0cb6f56ee54c3fdde88f3392caf0e0c3
root <fedejalife@gmail.com>
2022-09-27 18:45:25 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ int inta = 4;
 int longint = 4;
 int longlongint = 8;
 int floata = 4;
-	printf("Size of a char: %d byte(s)\n", chara);
-	printf("Size of an int: %d byte(s)\n", inta);
-	printf("Size of a long int: %d byte(s)\n", longint);
-	printf("Size of a long long int: %d byte(s)\n", longlongint);
-	printf("Size of a float: %d byte(s)\n", floata);
+	printf("Size of a char: %ld byte(s)\n",sizeof chara);
+	printf("Size of an int: %ld byte(s)\n",sizeof inta);
+	printf("Size of a long int: %ld byte(s)\n",sizeof longint);
+	printf("Size of a long long int: %ld byte(s)\n",sizeof longlongint);
+	printf("Size of a float: %ld byte(s)\n",sizeof floata);
 	return (0);
 }

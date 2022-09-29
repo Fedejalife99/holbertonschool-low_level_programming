9f5072ef1d99ad647800c512b5941580a394bd8c
root <fedejalife@gmail.com>
2022-09-29 10:20:57 -0700

    task8&9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b = ',';
+	int c = ' ';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a < '9')
+		{
+			putchar(b);
+		}
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}

d1c0b93d9eeea40af9fe863472aeb365b55cae81
root <fedejalife@gmail.com>
2022-09-30 15:31:16 -0700

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - the program begin
+*/
+
+void print_alphabet(void)
+
+{
+	char alf = 'a';
+
+	for (alf = 'a'; alf <= 'z'; alf++)
+	{
+		putchar(alf);
+	}
+	putchar('\n');
+}

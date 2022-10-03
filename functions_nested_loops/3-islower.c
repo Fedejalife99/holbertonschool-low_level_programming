1ceddd25284c5f0004d8d38e806de245cacadb34
root <fedejalife@gmail.com>
2022-10-03 05:58:47 -0700

    task3&4

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_islower - returns the value
+ *
+ *@c: int
+ *Return: 0 , 1
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+
+	return (0);
+
+
+
+}
+
+
---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - Determina si el caracter es una letra
+ * Return: 0 1
+ *@c:int
+ */
+
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+return (0);
+}

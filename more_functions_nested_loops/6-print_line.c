b805e43062d12e019636961d4ecc4dd5a688ced8
root <fedejalife@gmail.com>
2022-10-05 07:31:28 -0700

    task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line - function
+ *@n: int
+ *
+ */
+
+void print_line(int n)
+{
+	int cont = 0;
+
+	for (cont = 0; cont <= n; cont++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}

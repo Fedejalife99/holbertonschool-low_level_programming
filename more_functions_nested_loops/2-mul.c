10e8c6f6506fcabd1e8060d2b308251cfd9646ce
root <fedejalife@gmail.com>
2022-10-05 06:11:11 -0700

    task2&3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ *mul - multiplica
+ *@a: int
+ *@b: int
+ *Return: result a*b
+ */
+
+#include "main.h"
+
+int mul(int a, int b)
+{
+	int i = a * b;
+
+	return (i);
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numers
+ *
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}

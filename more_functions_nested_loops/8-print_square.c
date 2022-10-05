648a7571b3afe7c4e5335563e9298fd5d0d9de10
root <fedejalife@gmail.com>
2022-10-05 12:00:22 -0700

    task8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_diagonal - function
+ *@n: int
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y < size; y++)
+			{
+				_putchar(35);
+
+			}
+			_putchar(35);
+			_putchar('\n');
+
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

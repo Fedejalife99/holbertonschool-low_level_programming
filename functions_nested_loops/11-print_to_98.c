3942f6c7ce23ee5feec9fcce96444c44ecdd5fa6
root <fedejalife@gmail.com>
2022-10-05 16:22:48 -0700

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n++;
 		}
 	}
@@ -19,13 +19,14 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d, ",n--);
+			printf("%d, ", n--);
 		}
-	} if (n == 98)
+	}
+	if (n == 98)
 	{
 		printf("98\n");
 	}
-					
+
 
 
 

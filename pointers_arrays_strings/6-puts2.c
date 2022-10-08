bb3c88b38ece17544498f3225994102149baecfb
root <fedejalife@gmail.com>
2022-10-08 16:01:53 -0700

    task6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *puts2 - function
+ *@str: parameter
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+		i++;
+
+	}
+	_putchar('\n');
+
+}
+

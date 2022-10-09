cd648b87986c4bfda9b988e82db462e108fb5034
root <fedejalife@gmail.com>
2022-10-09 11:42:50 -0700

    task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_array - function that prints ints
+ *
+ *@a: array of ints
+ *@n: number of ints
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+			printf("\n");
+		}
+		else
+		printf("%d, ", a[i]);
+		i++;
+	}
+}
+

4e127a3811e17c9b7f5cae9860609fe4b4efdc28
root <fedejalife@gmail.com>
2022-10-11 10:51:49 -0700

    task0.3

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = 0;
-	int o = 0;
 	
-	while (dest[i] <= '\0')
+	int j = 0;
+	
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[j] <= '\0')
+	while (src[j] != '\0')
 	{
+		dest[i] = src [j];
 		j++;
-	}
-	while (o < i + j)
-	{
-		dest[i] = src[j];
 		i++;
-		j++;
 	}
 	return (dest);
 }

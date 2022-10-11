5ace03e240ce5eb723b0a25b44f4eec3612ae8b8
root <fedejalife@gmail.com>
2022-10-10 17:10:02 -0700

    task0.2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,29 @@
 
 /**
  *_strcat - function that concatenate two strings
- *@
- *@
+ *@dest: pointer char
+ *@src: póinter char
  */
 char *_strcat(char *dest, char *src)
-{	
-	*src = *dest + 1;
-
+{
+	int i = 0;
+	int j = 0;
+	int o = 0;
+	
+	while (dest[i] <= '\0')
+	{
+		i++;
+	}
+	while (src[j] <= '\0')
+	{
+		j++;
+	}
+	while (o < i + j)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	return (dest);
 }
 

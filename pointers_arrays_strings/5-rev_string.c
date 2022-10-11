3cb3247c39a7deef82bfa93d7a5530e805bb08f2
root <fedejalife@gmail.com>
2022-10-11 05:57:18 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - prints a string in reverse
+ * @s: char
+ */
+
+void rev_string(char *s)
+{
+	int q = 0; 
+	int p = 0;
+	char i;
+
+	while (s[p] != '\0')
+	{
+		p++;
+	}
+	
+	while (p > q)
+	{
+		i = s[p];
+		s[p] = s[q];
+		s[q] = i;
+		p--;
+		q++;
+	}
+	printf("%s", s);
+}
+
+			

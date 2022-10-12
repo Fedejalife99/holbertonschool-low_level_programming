a730e752462312bc0fa7bd937b5dd068cb82a8e1
root <fedejalife@gmail.com>
2022-10-12 10:27:26 -0700

    task5.1

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *string_toupper - function
+ *@*: char
+ *Return: char
+ *@c: char
+ */
+
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] -= 32;
+
+		}
+		i++;
+	}
+	return (c);
+}

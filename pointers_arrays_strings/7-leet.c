ef152d3291b888791dea07e80aae60d6e14fadc0
root <fedejalife@gmail.com>
2022-10-12 15:49:37 -0700

    task7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *leet - function
+ *@s: char
+ *Return: s
+ */
+
+char *leet(char *s)
+{
+	int i;
+	int x;
+	char l[] = {'A', 'a', 'E', 'e', 'L', 'l', 'O', 'o', 'T', 't'};
+	int t[] = {'4', '4', '3', '3', '0', '0', '1', '1', '7', '7'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (s[i] == l[x])
+			{
+				s[i] = t[x];
+			}
+		}
+	}
+	return (s);
+}

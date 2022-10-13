52b225d16d6d08d395e912d3034a9cac3ee708d1
root <fedejalife@gmail.com>
2022-10-13 07:19:05 -0700

    task1.4

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,15 +11,15 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
+
 	char *p = dest;
 
 	for (i = 0; i < n; i++)
-	{	
+	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
 	return (p);
-}	
+}
 

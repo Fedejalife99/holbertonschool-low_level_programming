946e8a4b24872a8b13ae69e7191df8cd4d695e06
root <fedejalife@gmail.com>
2022-10-13 09:51:19 -0700

    task2.4

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
+ *_strchr - function
+ *@s: char
+ *@c: char
+ *Return: s or NULL
  *
- *
- */
+*/
 
 char *_strchr(char *s, char c)
 {
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-
-		if (s[i] == c)
+		if (*s == c)
 		{
 			return (s);
 		}
-	
+		s++;
 	}
-	return ('\0');
+	return (NULL);
+
 
 }
 

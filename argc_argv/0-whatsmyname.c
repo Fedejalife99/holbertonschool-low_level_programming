10c0d86635ee2a923e9b1432728036ce852246b2
root <fedejalife@gmail.com>
2022-10-17 07:26:45 -0700

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
  *main - function
  *@argc: int
  *@argv: char
- *
+ *Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
+	(void) argc;
 
-	printf("%s\n", argv [0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }

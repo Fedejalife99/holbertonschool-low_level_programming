bd335fac753d476e4ff6b183167ec7da9099fca2
root <fedejalife@gmail.com>
2022-10-17 09:09:20 -0700

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ */
+
+int main(int argc, char *agrv[])
+{
+	(void) agrv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

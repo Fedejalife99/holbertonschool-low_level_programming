873deb941198d2af2ad481d7ddba3b90ba763222
root <fedejalife@gmail.com>
2022-10-18 09:57:07 -0700

    task4.5

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,19 +14,22 @@ int main(int argc, char *argv[])
 
 	int j = 0;
 
-	int result = 0;
+	int add = 0;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 1; *(argv[i] + j) != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (*(argv[i] + j) >= 'A' && (*(argv[i]  + j) <= 'z'))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+		add += atoi(argv[i]);
 	}
+	printf("%d\n", add);
+	return (0);
 
 }
 

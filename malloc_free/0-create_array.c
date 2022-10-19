64ce3e778977b1de8dc0d101dc3be1292b3e8248
root <fedejalife@gmail.com>
2022-10-19 09:30:49 -0700

    task0.2

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,21 +6,21 @@
  *create_array - function
  *@size: unisgned int
  *@c: char
- *Return: 0 if size or c are 0, NULL; h if last thing dont happnend and NULL if there is an error.
+ *Return: 0 , NULL or h
  */
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int a = 0;
-	
-	char* h = malloc(sizeof(char) * size);
-	
-	if (size == 0 )
+
+	char *h = malloc(sizeof(char) * size);
+
+	if (size == 0)
 	{
 		return (0);
 	}
 
-	
+
 	for (a = 0; a < size; a++)
 	{
 		h[a] = c;

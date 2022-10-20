a3939bd5d1d0fc79ef7d20754e3d791c665ee632
root <fedejalife@gmail.com>
2022-10-20 15:08:08 -0700

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,10 +21,12 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 	{
 		return (NULL);
+		free(p);
 	}
 	if (p == NULL)
 	{
 		return (NULL);
+		free(p);
 	}
 
 	for (i = 0; i < width; i++)

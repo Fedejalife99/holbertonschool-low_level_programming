481dab671371dbc00c58b81a0275fa4158b4a940
root <fedejalife@gmail.com>
2022-10-20 06:57:28 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 
 	unsigned int j;
 
-	char *h = malloc(sizeof(char) * (strlen(s1) + strlen(s2)));
+	char *h = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 
 	if (s1 == NULL || s2 == NULL)
 	{

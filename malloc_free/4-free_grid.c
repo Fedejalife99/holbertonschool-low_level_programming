4d10a84aac06de123cb835a573e5052ae5ea64e1
root <fedejalife@gmail.com>
2022-10-21 05:14:12 -0700

    task4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *free_grid - function
+ *@grid: int
+ *@height: int
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	
+	free(grid);
+}

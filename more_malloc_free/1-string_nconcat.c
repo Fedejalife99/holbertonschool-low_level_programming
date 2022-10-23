f4236d82c08ad49c4dd187208a3f9ba690d1c84a
root <fedejalife@gmail.com>
2022-10-23 12:35:18 -0700

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	unsigned int j;
 
-	char *h = malloc(sizeof(char) * strlen(s1) + strlen(s2));
+	char *h = malloc(sizeof(char) * strlen(s1) + n);
 
 	if (strlen(s1) == 0 || strlen(s2) == 0)
 	{

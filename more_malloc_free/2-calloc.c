6c9cc18f5b8db433b413ff5f05ba22f6636c14a9
root <fedejalife@gmail.com>
2022-10-24 05:27:04 -0700

    task2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 
-	int *p = malloc(size * nmemb);
+	int *p = malloc(sizeof(size) * nmemb);
 
 	if (nmemb == 0 || size <= 0)
 		return (NULL);

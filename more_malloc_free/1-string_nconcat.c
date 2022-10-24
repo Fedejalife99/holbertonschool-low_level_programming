95b1c863cccfa49000479c7128104ec4c513f155
root <fedejalife@gmail.com>
2022-10-24 09:53:59 -0700

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,6 +26,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		if (s2 == 0)
 			s2 = "";
 	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (h == NULL)
 		return (NULL);
 	if (h == 0)

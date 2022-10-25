49009ae6594a130fa36193b519aab1aaf8507441
root <fedejalife@gmail.com>
2022-10-25 06:19:50 -0700

    task1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,16 +9,21 @@
  */
 char *_strdup(char *str)
 {
-	char *h = malloc((sizeof(char) * strlen(str)) + 1);
+	char *h = NULL;
 
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (str == NULL)
-		return ('\0');
+		return (NULL);
+
+	h = malloc((sizeof(char) * strlen(str)) + 1);
+
+	i = 0;
+
 	if (h == NULL)
 		return (NULL);
 	if (*str == '\0')
-	{	
+	{
 		*h = '\0';
 		return (h);
 	}

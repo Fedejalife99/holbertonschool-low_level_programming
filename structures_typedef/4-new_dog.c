ef99300e47f64b9caff78fb448c7938550d84332
root <fedejalife@gmail.com>
2022-10-27 10:58:49 -0700

    task4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *new_dog - function that creates a new dog
+ *@name: pointer to a char
+ *@age: float
+ *@owner: pointer to char
+ *Return: caniche
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *caniche = malloc(sizeof(dog_t));
+
+	if (age <= 0)
+		free(caniche);
+	if (caniche == NULL)
+	{
+		free(caniche);
+	}
+	caniche->name = malloc(sizeof(char) * strlen(name));
+	if (caniche->name == NULL)
+	{
+		free(caniche->name);
+		free(caniche);
+	}
+	caniche->owner = malloc(sizeof(char) * strlen(name));
+	if (caniche->owner == NULL)
+	{
+		free(caniche->owner);
+		free(caniche);
+	}
+
+	caniche->name = strcpy(caniche->name, name);
+	caniche->owner = strcpy(caniche->owner, owner);
+	caniche->age = age;
+
+	return (caniche);
+}
+
+

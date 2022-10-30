ae523d4b8d28b26a6b00c09b885a85a2474ef295
root <fedejalife@gmail.com>
2022-10-30 09:47:17 -0700

    task1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - function
+ *@array: array of functions
+ *@size: size_t
+ *@action: pointer to a function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; array[i] < array[size]; i++)
+		action(array[i]);
+}

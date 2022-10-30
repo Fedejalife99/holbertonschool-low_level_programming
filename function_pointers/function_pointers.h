7881e4a5653d9253b40e71fd3cac55df3908344d
root <fedejalife@gmail.com>
2022-10-30 09:16:10 -0700

    function_pointers.fix

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,9 @@
 #include<stdio.h>
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-
+#endif

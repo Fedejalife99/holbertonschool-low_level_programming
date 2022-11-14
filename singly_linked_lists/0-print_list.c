f34821dc2545e4a9f13ef6e30135aacb9dacb9be
Fedejalife99 <fedejalife@gmail.com>
2022-11-14 11:06:06 -0800

    task0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include"lists.h"
+/**
+ * print_list - function that print elements of a node
+ * @h: head of node
+ * Return: value of i
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	const list_t *lista;
+
+	for (lista = h; lista != NULL; lista = lista->next)
+	{
+		if (lista->str == NULL)
+		{
+			printf("[0] nil\n");
+			i++;
+			lista = lista->next;
+		}
+		printf("[%d] ", lista->len);
+		printf("%s\n", lista->str);
+		i++;
+
+	}
+
+	return (i);
+}
+

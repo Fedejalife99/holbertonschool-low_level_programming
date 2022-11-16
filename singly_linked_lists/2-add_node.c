da0e2b0273c68bedc500679e48ccfe1eb9691dc1
Fedejalife99 <fedejalife@gmail.com>
2022-11-16 12:50:45 -0800

    task2 fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,7 +11,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *new = malloc(sizeof(list_t));
-
+	
+	if (new == NULL)
+		return (NULL);
 	if (str == NULL)
 		return (NULL);
 	if (head  == NULL)

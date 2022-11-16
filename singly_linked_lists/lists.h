bc0cda226d8649248f757b803ec7533a2a437b9c
Fedejalife99 <fedejalife@gmail.com>
2022-11-16 11:54:01 -0800

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define lists_h
 #include <stdio.h>
 #include <stdlib.h>
-#endif
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,3 +22,4 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+#endif

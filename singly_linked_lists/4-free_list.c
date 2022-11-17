466cf2b94f28ba4cad5111202e3f5021816e8953
Fedejalife99 <fedejalife@gmail.com>
2022-11-17 04:18:05 -0800

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "list.t"
+#include <stdlib.h>
+
+/**
+ * free_list - fuction that free a list
+ * @head: head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *frii;
+
+	while (head->next)
+	{
+		frii = head;
+		head = head->next;
+		free(frii->str)
+		free(frii);
+	}
+}
+
+

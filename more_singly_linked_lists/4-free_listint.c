ef21b74c1a09322fc3dba2311284a36437ef3b66
Fedejalife99 <fedejalife@gmail.com>
2022-11-17 06:36:50 -0800

    task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - fuction that free a list
+ * @head: head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *frii;
+
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		frii = head;
+		head = head->next;
+		free(frii);
+	}
+	if (head->next == NULL)
+	{
+		frii = head;
+		head = head->next;
+		free(frii);
+	}
+
+}

f0bc37bb98fc33081d8a786b99c62019bd96db69
Fedejalife99 <fedejalife@gmail.com>
2022-11-17 05:08:24 -0800

    task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_listint - function that prints the elements of a node
+ * @h: pointer to the first struct of the list
+ * Return: i
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	const listint_t *lista;
+
+	for (lista = h; lista->next != NULL; lista = lista->next)
+	{
+		printf("%d\n", lista->n);
+		i++;
+	}
+
+	if (lista->next == NULL)
+	{
+		printf("%d\n", lista->n);
+		i++;
+	}
+	return (i);
+}

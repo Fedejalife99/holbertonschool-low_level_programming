7b731fcd4b3641c51474993b8f88c8d5ca189b8e
Fedejalife99 <fedejalife@gmail.com>
2022-11-18 09:05:01 -0800

    task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -3,6 +3,7 @@
  * get_nodeint_at_index - function that returns the nth node of a linked list
  * @head: pointer to a struct
  * @index: index of the node
+ * Return: head
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -15,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			return (NULL);
 		head = head->next;
 	}
-	
+
 	return (head);
-}	
+}
 

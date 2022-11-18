bc41b8e134a74b6d6d5e1347f526c51b13f1036f
Fedejalife99 <fedejalife@gmail.com>
2022-11-18 14:58:25 -0800

    task 9 last fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,19 +15,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *new = malloc(sizeof(listint_t));
 
+	if (new == NULL)
+		return (NULL);
+
 	aux = *head;
-	
+
 	new->n = n;
 
 	if (head == NULL)
-	{	
+	{
 		new->n = n;
 		aux->next = new;
 		return (new);
 	}
-	
-	if (new == NULL)
-		return (NULL);
 
 	if (idx == 0)
 	{

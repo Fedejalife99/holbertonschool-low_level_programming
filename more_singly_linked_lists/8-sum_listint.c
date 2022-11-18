3332a3f741f30137f6bc6491af2e14cb7bad4cc6
Fedejalife99 <fedejalife@gmail.com>
2022-11-18 09:22:06 -0800

    task 8

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -10,8 +10,11 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
-	if (head->next == NULL)
+	if (head == NULL)
 		return (0);
+	if (head->next == NULL)
+		sum += head->n;
+		return (sum);
 	while (head->next != NULL)
 	{
 		sum += head->n;

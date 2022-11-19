9a7809d710b3970387ca25cd6d3215907e387f17
Fedejalife99 <fedejalife@gmail.com>
2022-11-19 10:44:47 -0800

    task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_unit - function that prints the binary representation of a number
+ * @b: const char
+ * return: number in base 10
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+
+	int sum = 0;
+
+	int po = 1;
+
+	int j = strlen(b);
+
+	if (b == NULL)
+		return (0);
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		sum += ((b[i] - '0') * po);
+
+		po *= 2;
+
+	}
+
+	return (sum);
+}

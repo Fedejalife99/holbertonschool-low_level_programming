41e4413ee1e026931c796c0dd40b3c1dce1f5cd8
Fedejalife99 <fedejalife@gmail.com>
2022-11-22 15:25:36 -0800

    task 3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - function that frees a list
+ * @head: pointer to a pointer to a struct
+ * @n: const int
+ * Return: new
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
+
+	last = *head;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	if (last != NULL)
+	{
+		for (last = *head; last->next != NULL; last = last->next)
+			continue;
+		new->prev = last;
+		last->next = new;
+	}
+
+	return (new);
+}

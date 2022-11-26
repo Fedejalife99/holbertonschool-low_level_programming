7c437ee4afc9d19dfa9b1e9b3d6fceef5e9e2bca
Fedejalife99 <fedejalife@gmail.com>
2022-11-26 05:37:31 -0800

    task 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * key_index -  function that gives you the index of a key
+ * @key: key of the node
+ * @size: size of the hash table
+ * Return: index;
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{	
+	return (hash_djb2(key) % size);
+}
+
+
